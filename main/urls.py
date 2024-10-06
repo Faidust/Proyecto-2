4366c8a6fe0589f736611be0ec021f2742556efd
Faidust <tbgp1990@gmail.com>
2024-10-06 14:20:39 -0300

    hola papu

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns=[
-    path('', views.hello),
+    path('', views.index),
     path("about/", views.about)
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
 
 def hello(request):
     return HttpResponse("Hello World hola")
 
 def about(request):
-    return HttpResponse("About")
+    return render(request, "About.html")
