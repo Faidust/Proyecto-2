1b7abe5964cd9573a4d07734f224051598892450
PapuNeo <lucasgh777@gmail.com>
2024-10-05 22:31:48 -0300

    hola

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Hello World hola")
 
 def about(request):
     return HttpResponse("About")
