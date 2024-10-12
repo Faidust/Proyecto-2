0836c83d16d65cc6c8a385892317ece040459464
Faidust <tbgp1990@gmail.com>
2024-10-11 22:20:24 -0300

    boton para inicio de sesion desde la pagina principal

--- main/views.py
+++ main/views.py
@@ -6,8 +6,5 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
-def hello(request):
-    return HttpResponse("Hello World hola")
-
 def about(request):
     return render(request, "About.html")
---
+++ usuario/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def usuario(request):
+    return render(request, "usuario.html")
