28cc0f43d9f77f4e8986fa35cf33c89a31dad2f2
Faidust <tbgp1990@gmail.com>
2024-10-13 17:37:58 -0300

    login y logout

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("usuario/", views.usuario)
+    path("usuario/", views.usuario),
+    path("logout/", views.exit),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 @login_required
 def usuario(request):
     return render(request, "usuario.html")
+
+def exit(request):
+    logout(request)
+    return redirect("home")
