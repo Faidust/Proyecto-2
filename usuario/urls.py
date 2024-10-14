3f749e3273e08c406e39c9677f74f141b506b150
Faidust <tbgp1990@gmail.com>
2024-10-13 21:52:24 -0300

    Crear Registro de usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("usuario/", views.usuario),
+    path("usuario/", views.usuario, name="usuario"),
     path("logout/", views.exit),
+    path("register/", views.register, name="register"),
 ]
